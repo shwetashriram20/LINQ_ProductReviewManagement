3a72ae28721cfa08a51089c69d8a38791b04d575
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-05 22:14:06 +0530

    Skip Top 5 Records & Display

---
+++ Management.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Management
+    {
+        public static void TopThreeByRating(List<ProductReview> productReview)
+        {
+
+            var Record = (from productreview in productReview orderby productreview.Rating descending select productreview);
+
+            foreach (var element in Record)
+            {
+                Console.WriteLine("ProductID :{0}, UserID :{1}, Rating :{2}, Review :{3}, isLike :{4}", element.ProductID, element.UserID, element.Rating, element.Review, element.isLike);
+            }
+        }
+
+        public static void RetrieveAllRecords(List<ProductReview> productReview)
+        {
+
+            var Record = from productreview in productReview
+                         where (productreview.ProductID == 1 || productreview.ProductID == 4 || productreview.ProductID == 9)
+                         && productreview.Rating > 3
+                         select productreview;
+
+            foreach (var element in Record)
+            {
+                Console.WriteLine("ProductID :{0}, UserID :{1}, Rating :{2}, Review :{3}, isLike :{4}", element.ProductID, element.UserID, element.Rating, element.Review, element.isLike);
+            }
+        }
+
+        public static void retriveCountOfReviewForEachProductID(List<ProductReview> productReview)
+        {
+
+            var Record = productReview.GroupBy(x => x.ProductID).Select(x => new { productid = x.Key, count = x.Count() });
+
+            foreach (var element in Record)
+            {
+                Console.WriteLine("ProductID :{0}, Count :{1}", element.productid, element.count);
+            }
+        }
+
+        public static void retriveOnlyProductIDAndReview(List<ProductReview> productReview)
+        {
+            var Records = from productreview in productReview select productreview;
+
+            foreach (var element in Records)
+            {
+                Console.WriteLine("ProductID :{0},  Review :{1}", element.ProductID, element.Review);
+            }
+        }
+
+        public static void skipTopFiveRecordAndDisplayOther(List<ProductReview> productReview)
+        {
+            var Records = (from productreview in productReview select productreview).Skip(5);
+
+            foreach (var element in Records)
+            {
+                Console.WriteLine("ProductID :{0},  Review :{1}", element.ProductID, element.Review);
+            }
+        }
+    }
+}
