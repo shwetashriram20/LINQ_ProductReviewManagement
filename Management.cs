3d9ea5daaeacabfdb19cc9798ddef4f84b8a680f
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-05 21:44:18 +0530

    Management file

--- Management.cs
+++ Management.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ProductReviewManagement
 {
-    internal class Management
+    public class Management
     {
+        public static void TopThreeByRating(List<ProductReview> productReview)
+        {
+
+            var Record = (from productreview in productReview orderby productreview.Rating descending select productreview).Take(3);
+
+            foreach (var element in Record)
+            {
+                Console.WriteLine("ProductID :{0}, UserID :{1}, Rating :{2}, Review :{3}, isLike :{4}", element.ProductID, element.UserID, element.Rating, element.Review, element.isLike);
+            }
+        }
     }
 }
